8f16a6761dcf151e00b5dff21c5257ea8432c5c1
Pedropcrdp242 <106442286+Pedropcrdp242@users.noreply.github.com>
2024-11-28 19:55:02 -0300

    UPDATE EvangeOS issues

--- overlay/NtOverlays/Android.mk
+++ overlay/NtOverlays/Android.mk
@@ -12,14 +12,14 @@ OVERLAY_DIRS := \
 
 # Para cada diretório, inclua o APK correspondente
 $(foreach dir, $(OVERLAY_DIRS), \
-    include $(CLEAR_VARS) \
-    LOCAL_MODULE := $(dir) \
-    LOCAL_SRC_FILES := $(dir)/$(dir).apk \
-    LOCAL_MODULE_CLASS := APPS \
-    LOCAL_CERTIFICATE := PRESIGNED \
-    LOCAL_MODULE_SUFFIX := .apk \
-    LOCAL_MODULE_TAGS := optional \
-    LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_OVERLAY) \
-    include $(BUILD_PREBUILT) \
+    $(eval include $(CLEAR_VARS)) \
+    $(eval LOCAL_MODULE := $(dir)) \
+    $(eval LOCAL_SRC_FILES := $(dir)/$(dir).apk) \
+    $(eval LOCAL_MODULE_CLASS := APPS) \
+    $(eval LOCAL_CERTIFICATE := PRESIGNED) \
+    $(eval LOCAL_MODULE_SUFFIX := .apk) \
+    $(eval LOCAL_MODULE_TAGS := optional) \
+    $(eval LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_OVERLAY)) \
+    $(eval include $(BUILD_PREBUILT)) \
 )
 
